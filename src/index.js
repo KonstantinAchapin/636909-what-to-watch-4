0b4da6979391c322b32d17ddce4aa60d8853ee5b
Konstantin <KonstantinAchapin@mail.ru>
2020-06-12 14:44:21 +0200

    Отображение main с добавлением props

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import {App} from './components/app.jsx';
+
+const init = () => {
+  const movieOptions = {
+    title: `Hotel Grand Budapest`,
+    genre: `Adventure`,
+    releaseDate: `2014`
+  };
+
+  ReactDOM.render(<App title={movieOptions.title} genre={movieOptions.genre} releaseDate={movieOptions.releaseDate} />, document.getElementById(`root`));
+};
+
+init();
