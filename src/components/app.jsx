06b61eafc47402931621ea9a392152887340e037
Konstantin <KonstantinAchapin@mail.ru>
2020-06-13 13:34:33 +0200

    Добавляет компонент отрисовки списка фильмов

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import {Main} from './main.jsx';
 
+
 export const App = (props) => {
   // eslint-disable-next-line react/prop-types
-  const {title, genre, releaseDate} = props;
-  return <Main title={title} genre={genre} releaseDate={releaseDate}/>;
+  const {title, genre, releaseDate, filmsTitle} = props;
+  return <React.Fragment>
+    <Main title={title} genre={genre} releaseDate={releaseDate} filmsTitle={filmsTitle}/>;
+  </React.Fragment>;
 };
--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {App} from './components/app.jsx';
 
+
 const init = () => {
   const movieOptions = {
     title: `Hotel Grand Budapest`,
     genre: `Adventure`,
-    releaseDate: `2014`
+    releaseDate: `2014`,
+    filmsTitle: [`Fantastic Beasts`, `Bohemian Rhapsody`, `Macbeth`, `Godfather`, `Inception`, `Fight Club`, `Parasite`, `Leon`]
   };
 
-  ReactDOM.render(<App title={movieOptions.title} genre={movieOptions.genre} releaseDate={movieOptions.releaseDate} />, document.getElementById(`root`));
+  ReactDOM.render(<App title={movieOptions.title} genre={movieOptions.genre} releaseDate={movieOptions.releaseDate} filmsTitle={movieOptions.filmsTitle} />, document.getElementById(`root`));
 };
 
 init();
